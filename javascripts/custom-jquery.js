18214620904fcc918522c983c2e2e51fc5404f6b
Travis Thielen <travis@stuxweb.com>
2016-01-30 11:07:12 -1000

    Add secure email link via custom jquery file

---
+++ javascripts/custom-jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	var first = "travis";
+    var last = "stuxweb.com";
+    $('.email-contact-link').html('<a class="link-text" href="mailto:'+first + '@' + last+'">'+first + '@' + last+'<\/a>');
+
+    $('.email-contact-reg').html('<a href="mailto:'+first + '@' + last+'">'+first + '@' + last+'<\/a>');
+
+    $('.email-contact-sr').html('<a href="mailto:'+first + '@' + last+'">Travis Thielen | Email<\/a>');
+
+});
